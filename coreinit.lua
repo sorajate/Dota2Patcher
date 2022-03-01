230ce82c838245bdd1e20982daa8e2d37f4fa892
Alexander Wolf <fake.wolf@2ch.hk>
2022-03-01 19:04:28 +1000

    Update coreinit.lua

--- coreinit.lua
+++ coreinit.lua
@@ -1,5 +1,5 @@
 local EnableConvars = function(s)
-  local cmd = "sv_cheats 1; dota_use_particle_fow 0; fog_enable 0; fow_client_nofiltering 1; dota_camera_distance 1500; r_farz 3000;";
+  local cmd = "sv_cheats 1; dota_use_particle_fow 0; fog_enable 0; dota_camera_distance 1500; r_farz 3000;";
   if SendToServerConsole then SendToServerConsole(cmd) else SendToConsole(cmd) end
 end
 
